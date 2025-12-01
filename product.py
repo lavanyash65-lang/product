3fee008ac6d6c9b86b253d0548b79db58c9d7aa7
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-01 16:15:59 +0530

    Update product.py

--- product.py
+++ product.py
@@ -6,7 +6,7 @@ def product_info(product_id, name, quantity, price):
         f"Price: {price}"
     )
 
-if name == "main":
+if __name__ == "__main__":
     product_id = "P102"
     name = "Keyboard"
     quantity = 5
