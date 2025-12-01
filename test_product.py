3b776abec91bce560092c19b6b2ac1ac1c700411
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-01 16:13:35 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,10 +1,11 @@
-from product import product_details
-def test_product_details():
+from product import product_info
+
+def test_product_info():
     expected_output = (
-        "product ID: E1001\n "
-        "product name: alice\n "
-        "quantity: 2\n "
-        "price: 50000"
+        "Product ID: P102\n"
+        "Name: Keyboard\n"
+        "Quantity: 5\n"
+        "Price: 799.5"
     )
-    assert product_details("E1001", "alice", 2, 55000) == expected_output
-        
+
+    assert product_info("P102", "Keyboard", 5, 799.5) == expected_output
